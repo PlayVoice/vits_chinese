7ffebd06720512f125b1594ef6c6209163b82c36
shizihao <shizihao@softstar.net.cn>
2023-03-22 17:00:49 +0800

    支持单人自定义数据集，格式兼容moegoe;支持windows训练和推理;更好的符号支持。

--- vits_pinyin.py
+++ vits_pinyin.py
@@ -1,9 +1,12 @@
 import re
 
+import pypinyin
 from pypinyin import Style
 from pypinyin.contrib.neutral_tone import NeutralToneWith5Mixin
 from pypinyin.converter import DefaultConverter
 from pypinyin.core import Pinyin
+
+import numpy as np
 
 from text import pinyin_dict
 from bert import TTSProsody
@@ -38,51 +41,81 @@ class VITS_PinYin:
         self.pinyin_parser = Pinyin(MyConverter())
         self.prosody = TTSProsody(bert_path, device)
 
-    def get_phoneme4pinyin(self, pinyins):
-        result = []
-        count_phone = []
-        for pinyin in pinyins:
-            if pinyin[:-1] in pinyin_dict:
-                tone = pinyin[-1]
-                a = pinyin[:-1]
-                a1, a2 = pinyin_dict[a]
-                result += [a1, a2 + tone]
-                count_phone.append(2)
-        return result, count_phone
+    # def get_phoneme4pinyin(self, pinyins):
+    #     result = []
+    #     count_phone = []
+    #     for pinyin in pinyins:
+    #         if pinyin[:-1] in pinyin_dict:
+    #             tone = pinyin[-1]
+    #             a = pinyin[:-1]
+    #             a1, a2 = pinyin_dict[a]
+    #             result += [a1, a2 + tone]
+    #             count_phone.append(2)
+    #     return result, count_phone
 
     def chinese_to_phonemes(self, text):
-        text = clean_chinese(text)
-        phonemes = ["sil"]
-        chars = ['[PAD]']
-        count_phone = []
-        count_phone.append(1)
-        for subtext in text.split(","):
-            if (len(subtext) == 0):
-                continue
-            pinyins = self.correct_pinyin_tone3(subtext)
-            sub_p, sub_c = self.get_phoneme4pinyin(pinyins)
-            phonemes.extend(sub_p)
-            phonemes.append("sp")
-            count_phone.extend(sub_c)
+        # @todo:考虑使用g2pw的chinese bert替换原始的pypinyin,目前测试下来运行速度太慢。
+        # 将标准中文文本符号替换成 bert 符号库中的单符号,以保证bert的效果.
+        text = text.replace("——", "...")\
+            .replace("—", "...")\
+            .replace("……", "...")\
+            .replace("…", "...")\
+            .replace('“', '"')\
+            .replace('”', '"')\
+            .replace("\n", "")
+        tokens = self.prosody.char_model.tokenizer.tokenize(text)
+        assert not tokens.count("[UNK]")
+        pinyins = np.reshape(pypinyin.pinyin(text, style=pypinyin.TONE3), (-1))
+        try:
+            phone_index = 0
+            phone_items = []
+            phone_items.append('sil')
+            count_phone = []
             count_phone.append(1)
-            chars.append(subtext)
-            chars.append(',')
-        phonemes.append("sil")
-        count_phone.append(1)
-        chars.append('[PAD]')
-        chars = "".join(chars)
-        char_embeds = self.prosody.get_char_embeds(chars)
-        char_embeds = self.prosody.expand_for_phone(char_embeds, count_phone)
-        return " ".join(phonemes), char_embeds
+            temp = ""
 
-    def correct_pinyin_tone3(self, text):
-        pinyin_list = [p[0] for p in self.pinyin_parser.pinyin(
-            text, style=Style.TONE3, strict=False, neutral_tone_with_five=True)]
-        if len(pinyin_list) >= 2:
-            for i in range(1, len(pinyin_list)):
-                try:
-                    if re.findall(r'\d', pinyin_list[i-1])[0] == '3' and re.findall(r'\d', pinyin_list[i])[0] == '3':
-                        pinyin_list[i-1] = pinyin_list[i-1].replace('3', '2')
-                except IndexError:
-                    pass
-        return pinyin_list
+            len_pys = len(tokens)
+            for word in tokens:
+                if is_chinese(word):
+                    count_phone.append(2)
+                    if (phone_index >= len_pys):
+                        print(f"!!!![{text}]plz check ur text whether includes MULTIBYTE symbol.(请检查你的文本中是否包含多字节符号)")
+                    pinyin = pinyins[phone_index]
+                    phone_index = phone_index + 1
+                    if not pinyin[-1].isdigit():
+                        pinyin += "5"
+                    if pinyin[:-1] in pinyin_dict:
+                        tone = pinyin[-1]
+                        a = pinyin[:-1]
+                        a1, a2 = pinyin_dict[a]
+                        phone_items += [a1, a2 + tone]
+                else:
+                    temp += word
+                    if temp == pinyins[phone_index]:
+                        temp = ""
+                        phone_index += 1
+                    count_phone.append(1)
+                    phone_items.append('sp')
+
+            count_phone.append(1)
+            phone_items.append('sil')
+            phone_items_str = ' '.join(phone_items)
+        except IndexError as e:
+            print('except:', e)
+
+        text = f'[PAD]{text}[PAD]'
+        char_embeds = self.prosody.get_char_embeds(text)
+        char_embeds = self.prosody.expand_for_phone(char_embeds, count_phone)
+        return phone_items_str, char_embeds
+
+    # def correct_pinyin_tone3(self, text):
+    #     pinyin_list = [p[0] for p in self.pinyin_parser.pinyin(
+    #         text, style=Style.TONE3, strict=False, neutral_tone_with_five=True)]
+    #     if len(pinyin_list) >= 2:
+    #         for i in range(1, len(pinyin_list)):
+    #             try:
+    #                 if re.findall(r'\d', pinyin_list[i - 1])[0] == '3' and re.findall(r'\d', pinyin_list[i])[0] == '3':
+    #                     pinyin_list[i - 1] = pinyin_list[i - 1].replace('3', '2')
+    #             except IndexError:
+    #                 pass
+    #     return pinyin_list
